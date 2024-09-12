921bf6287fc0ff139c2188f5e12b94f67c17fdf0
ZachJHansen <zachansen62@gmail.com>
2024-09-12 08:24:51 -0500

    adding ht simplifications

--- src/simplifying/fol/classic.rs
+++ src/simplifying/fol/classic.rs
@@ -1,6 +1,6 @@
 use crate::syntax_tree::fol::Theory;
 
 pub fn simplify(theory: Theory) -> Theory {
-    crate::simplifying::fol::ht::simplify(theory)
+    crate::simplifying::fol::ht::simplify_shallow(theory)
     // TODO: Add classic simplifications
 }
