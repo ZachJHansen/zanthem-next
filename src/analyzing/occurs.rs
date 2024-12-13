2fcf111c60335e6805d09039d7cf30c57d11c9e8
ZachJHansen <zachansen62@gmail.com>
2024-12-13 17:47:39 -0600

    adding tests

--- src/analyzing/occurs.rs
+++ src/analyzing/occurs.rs
@@ -7,7 +7,7 @@ use {
         },
     },
     itertools::{max, Itertools},
-    petgraph::graph::DiGraph,
+    petgraph::graph::{DiGraph, NodeIndex},
     std::collections::{HashMap, HashSet},
 };
 
@@ -169,17 +169,29 @@ impl Formula {
             Formula::QuantifiedFormula { formula, .. } => formula.atom_occurrences(occurrences),
         }
     }
+
+    pub fn cnf_pdg(&self, intensional: HashSet<Predicate>) -> DiGraph<(), ()> {
+        Theory {
+            formulas: vec![self.clone()],
+        }
+        .cnf_pdg(intensional)
+        .0
+    }
 }
 
 impl Theory {
-    // Return the vertex and edge sets corresponding to the PDG of SM & Circ Sec 6.2
-    pub fn cnf_pdg(&self, intensional: HashSet<Predicate>) -> DiGraph<(), ()> {
+    // Return the predicate dependency graph (SM & Circ Sec. 6.2)
+    // and the node mapping
+    pub fn cnf_pdg(
+        &self,
+        intensional: HashSet<Predicate>,
+    ) -> (DiGraph<(), ()>, HashMap<Predicate, NodeIndex>) {
         let mut dependency_graph = DiGraph::<(), ()>::new();
         let mut mapping = HashMap::new();
 
-        for predicate in intensional {
+        for predicate in intensional.iter() {
             let node = dependency_graph.add_node(());
-            mapping.insert(predicate, node);
+            mapping.insert(predicate.clone(), node);
         }
 
         for rule in self.formulas.clone() {
@@ -207,60 +219,37 @@ impl Theory {
                 _ => unreachable!(), //(x.rebox().clone(), x.rebox()), //unreachable!("Formula {rule} is not in Clark Normal Form"),
             };
 
-            //let counter = OccurrenceCounter::new();
-            //let _occurrences = antecedent.atom_occurrences(counter);
-
-            let _temp = Theory {
-                formulas: vec![antecedent, consequent],
-            };
+            if let Formula::AtomicFormula(AtomicFormula::Atom(atom)) = consequent {
+                let head_predicate = atom.predicate();
+                if intensional.contains(&head_predicate) {
+                    let occurrences = antecedent.atom_occurrences(OccurrenceCounter::new());
+                    for (occ, data) in occurrences {
+                        if !data.negated && (data.count % 2 == 0) {
+                            let body_predicate = occ.atom.predicate();
+                            if intensional.contains(&body_predicate) {
+                                dependency_graph.update_edge(
+                                    mapping[&head_predicate],
+                                    mapping[&body_predicate],
+                                    (),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        let formula: Formula = "(((p(a) -> p(b)) and p(b)) -> q(b)) or (p(a) -> q(b)) and p(a)"
-            .parse()
-            .unwrap();
-        let occurrences = formula.atom_occurrences(OccurrenceCounter::new());
-
-        for (occ, data) in occurrences {
-            println!(
-                "({}, {}): {} / {}",
-                occ.atom, occ.id, data.count, data.negated
-            );
-        }
-
-        dependency_graph
+        (dependency_graph, mapping)
     }
 }
 
-// impl Formula {
-//     // Subformula self is negated in formula
-//     fn negated(&self, formula: Formula) -> bool {
-//         todo!()
-//     }
-
-//     // Subformula self occurs positively, strictly positively, or non-positively in formula
-//     fn occurs(&self, formula: Formula) -> Occurrence {
-//         todo!()
-//     }
-// }
-
-// //But we need occurrences of a predicate, not the predicate itself...
-
-// impl Predicate {
-//     // Predicate self is negated in formula
-//     fn negated(&self, formula: Formula) -> bool {
-//         todo!()
-//     }
-
-//     // Predicate self occurs positively, strictly positively, or non-positively in formula
-//     fn occurs(&self, formula: Formula) -> Occurrence {
-//         todo!()
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
-    use super::{choose_unique_id, Occurrence, OccurrenceCounter, OccurrenceData};
-    use crate::syntax_tree::fol::Atom;
+    use {
+        super::{choose_unique_id, Occurrence, OccurrenceCounter, OccurrenceData},
+        crate::syntax_tree::fol::{Atom, Predicate, Theory},
+        std::collections::HashSet,
+    };
 
     #[test]
     fn test_choose_unique_id() {
@@ -316,4 +305,74 @@ mod tests {
     //         println!("({}, {}): {}", occ.atom, occ.id, count);
     //     }
     // }
+
+    #[test]
+    fn test_cnf_dpg() {
+        let mut intensional = HashSet::new();
+
+        let p0 = Predicate {
+            symbol: "p".into(),
+            arity: 0,
+        };
+        intensional.insert(p0.clone());
+
+        let q0 = Predicate {
+            symbol: "q".into(),
+            arity: 0,
+        };
+        intensional.insert(q0.clone());
+
+        let p1 = Predicate {
+            symbol: "p".into(),
+            arity: 1,
+        };
+        intensional.insert(p1.clone());
+
+        let q1 = Predicate {
+            symbol: "q".into(),
+            arity: 1,
+        };
+        intensional.insert(q1.clone());
+
+        // F1 contributes edge (p/0, q/0)
+        // F2 contributes edge (p/1, q/0)
+        // F3 contributes edge (q/1, p/0)
+        let theory: Theory = "q -> p. forall X ((q(X) -> q) -> p(X)). forall Y ( (forall X ((q(X) -> p(X)) -> p) ) -> q(Y) ).".parse().unwrap();
+
+        let (graph, map) = theory.cnf_pdg(intensional.clone());
+
+        assert!(graph.contains_edge(map[&p0], map[&q0]));
+        assert!(graph.contains_edge(map[&p1], map[&q0]));
+        assert!(graph.contains_edge(map[&q1], map[&p0]));
+        assert!(graph.contains_edge(map[&q1], map[&q1]));
+
+        assert_eq!(graph.node_count(), 4);
+        assert_eq!(graph.edge_count(), 4);
+
+        let r2 = Predicate {
+            symbol: "r".into(),
+            arity: 2,
+        };
+        intensional.insert(r2.clone());
+
+        let t2 = Predicate {
+            symbol: "t".into(),
+            arity: 2,
+        };
+        intensional.insert(t2.clone());
+
+        // F1 contributes no edges
+        // F2 contributes edge (p/0, p/0)
+        // F3 contributes no edges
+        // F4 contributes edge (r/2, t/2)
+        let theory: Theory = "forall X Y (r(X,Y) -> #false). (not q) and p -> p. (p -> #false) -> p. forall X Z (t(Z,X) and not t(X,Z) -> r(X,Z)).".parse().unwrap();
+
+        let (graph, map) = theory.cnf_pdg(intensional);
+
+        assert!(graph.contains_edge(map[&p0], map[&p0]));
+        assert!(graph.contains_edge(map[&r2], map[&t2]));
+
+        assert_eq!(graph.node_count(), 6);
+        assert_eq!(graph.edge_count(), 2);
+    }
 }
